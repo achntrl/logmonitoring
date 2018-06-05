b4769189a3f329cc230efe57ecc322a2c6de70ac
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2018-06-05 22:37:17 +0200

    Parse config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate logmonitoring;
+
+use std::env;
+use std::process;
+
+use logmonitoring::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Config parsed: {:?}", config)
 }
